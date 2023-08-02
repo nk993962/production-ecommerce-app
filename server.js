83ef7c014e3d811242020300fafd6fcf2d59ce25
nk993962 <128208286+nk993962@users.noreply.github.com>
2023-08-02 11:56:09 +0530

    updated

--- server.js
+++ server.js
@@ -16,9 +16,10 @@ dotenv.config();
 
 // database config
 connectDB();
+
 // esmodules fix
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const  __filename = fileURLToPath(import.meta.url);
+const  __dirname = path.dirname(__filename);
 
 // rest object
 const app = express();
